4e501f116398a41b83a28f1306707b34ef0ad016
DaveAldon <dcrawford@spotinc.com>
2020-11-26 14:38:49 -0500

    Added ble scanning functionality, using swiftui.

---
+++ Health Hub/Health_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_HubApp.swift
+//  Health Hub
+//
+//  Created by David Crawford on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Health_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
